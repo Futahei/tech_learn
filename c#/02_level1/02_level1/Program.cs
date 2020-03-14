382fa761877289c8d99a7a798bda087075588b8b
DESKTOP-N7Q5U4L\Futahei <futahei1108@icloud.com>
2020-03-14 20:40:17 +0900

    02_level1

---
+++ c#/02_level1/02_level1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace _02_level1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer;
+            ConsoleKeyInfo keyInfo;
+            bool isStop = false;
+
+            TimerCallback callback = state =>
+            {
+                if (!isStop)
+                {
+                    Console.Clear();
+                    Console.WriteLine(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssK"));
+                }
+            };
+
+            timer = new Timer(callback, null, 500, 1000);
+
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+
+                if (keyInfo.Key == ConsoleKey.S)
+                {
+                    isStop = !isStop;
+                }
+
+            }
+            while (keyInfo.Key != ConsoleKey.Enter);
+        }
+    }
+}
